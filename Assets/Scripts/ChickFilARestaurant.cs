1ccb15d616c01540a342c3c3a67595ba04375818
Dustin Milotte <dmilotte@gmail.com>
2020-11-21 21:06:04 -0500

    Add class example

---
+++ Assets/Scripts/ChickFilARestaurant.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickFilARestaurant : MonoBehaviour
+{
+    public string[] menuItems = {"Spicy Deluxe", "Original", "Grilled Chicken Wrap"};
+    public string greeting = "Hello, welcome to Chick Fil A";
+    
+    void Start()
+    {
+        TakeOrder();
+    }
+
+    private void TakeOrder()
+    {
+        Say(greeting);
+        EnterOrder();
+    }
+
+    private void DeliverOrderToTable()
+    {
+        // walk to the table
+        // set down the food
+    }
+    
+    private void EnterOrder()
+    {
+        throw new System.NotImplementedException();
+    }
+    private void Say(string greeting)
+    {
+        
+    }
+}
