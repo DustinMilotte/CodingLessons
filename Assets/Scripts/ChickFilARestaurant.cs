f2f8b013e2ee2602da997c584009973233736d4f
Dustin Milotte <dmilotte@gmail.com>
2020-12-05 19:34:08 -0500

    Add art and chick fil a greeting, classes scene

--- Assets/Scripts/ChickFilARestaurant.cs
+++ Assets/Scripts/ChickFilARestaurant.cs
@@ -1,15 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ChickFilARestaurant : MonoBehaviour
 {
     public string[] menuItems = {"Spicy Deluxe", "Original", "Grilled Chicken Wrap"};
     public string greeting = "Hello, welcome to Chick Fil A";
+    public GameObject greetingCanvas;
+    public TextMeshProUGUI greetingText;
     
     void Start()
     {
-        TakeOrder();
+        // TakeOrder();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayGreeting();
+    }
+
+    private void PlayGreeting()
+    {
+        greetingText.text = greeting;
+        greetingCanvas.SetActive(true);
     }
 
     private void TakeOrder()
